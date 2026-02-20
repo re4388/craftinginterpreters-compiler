ffc81ead79064f786ffaaf822d18e60f7bb1086e
ben.hu <ben.hu@wemoscooter.com>
2026-02-20 11:26:09 +0800

    feat: chunk of byte done

--- chunk.c
+++ chunk.c
@@ -9,21 +9,36 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
+    initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
+    freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code,
             oldCapacity, chunk->capacity);
+        chunk->lines = GROW_ARRAY(int, chunk->lines,
+        oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
+}
+
+int addConstant(Chunk* chunk, Value value) {
+    writeValueArray(&chunk->constants, value);
+
+    // we return the index where the constant was appended
+    // so that we can locate that same constant later.
+    return chunk->constants.count - 1;
 }
--- chunk.h
+++ chunk.h
@@ -1,9 +1,12 @@
 #ifndef clox_chunk_h
 #define clox_chunk_h
 
+
 #include "common.h"
+#include "value.h"
 
 typedef enum {
+    OP_CONSTANT,
     OP_RETURN,
   } OpCode;
 
@@ -13,12 +16,15 @@ typedef enum {
 // - Constant-time appending to the end of the array (amortized)
 typedef struct {
     uint8_t* code;
+    int* lines;  // Each number in the array is the line number for the corresponding byte in the bytecode.
+    ValueArray constants;
     int count;    // the number of elements are actually in use
     int capacity; // the number of elements in the array we have allocated
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
